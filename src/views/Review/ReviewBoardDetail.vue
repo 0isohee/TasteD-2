01da9a6973e8a71b36ee066631cced9c47cb1034
0isohee <dlthgml1361@naver.com>
2024-05-17 16:02:31 +0900

    0517 pm4:02

--- src/views/Review/ReviewBoardDetail.vue
+++ src/views/Review/ReviewBoardDetail.vue
@@ -42,6 +42,9 @@ export default {
         }
       }
     },
+    goToList() {
+      this.$router.push({ name: "ReviewBoard" });
+    },
   },
   watch: {
     // selectedReview 변경을 감지하여 작성자가 admin인지 여부를 다시 설정하는 메서드
@@ -120,7 +123,7 @@ export default {
               </div>
 
               <div class="btnContainer">
-                <v-btn color="#cab8a8" @click="goToList">글 목록 이동</v-btn>
+                <v-btn color="hover" @click="goToList">글 목록 이동</v-btn>
               </div>
 
               <div>
